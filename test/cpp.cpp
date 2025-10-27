4a6529960adf85bc3857420b27edddfd41f6f245
fdahk <3235159187@qq.com>
2025-10-27 06:10:20 -0700

    feat: new record

--- test/cpp.cpp
+++ test/cpp.cpp
@@ -1,128 +1,59 @@
 #include<iostream>
-#include<cstdlib>
+#include<cstring>
 using namespace std;
-
-typedef struct Node {
-    int data;
-    struct Node* next;
-}node;  //队列结点
- 
-//定义一个链队列
-typedef struct LinkQueue {
-    node* front; //队首结点
-    node* rear; //队尾结点
-}LQ;
- 
-//初始化空链队列
-LQ InitLQ(LQ LQ) {
-    LQ.front = (node*)malloc(sizeof(node));
-    LQ.front->data = -1;
-    LQ.front->next = NULL;
-    LQ.rear = LQ.front; //队首结点和队尾结点是同一个结点
-    return LQ;
-}
-
-//进栈，LS是栈顶结点
-node* PushStack(node* LS ,int elem) 
-{  
-    node* new_node = (node*)malloc(sizeof(node)); //创建一个结点
-    if (new_node == NULL) {
-        cout<<"创建链栈结点失败";
-        exit(0);
-    }
-    else {
-        new_node->data = elem;
-        new_node->next = LS; 
-        return new_node; // 返回新的栈顶结点
-    }
-}
-
-//入队列
-LQ PushQueue(LQ &LQ,int e) {
-        
-		node* new_node = (node*)malloc(sizeof(node));//生成新结点
-		if (new_node == NULL) {
-		        cout<<"创建结点失败";
-		        exit(0);
-		    }
-		new_node->data = e;
-        new_node->next = NULL;
-        LQ.rear->next = new_node; //在队尾结点处插入新结点
-        LQ.rear = new_node;//队尾结点后移
-        return LQ;
-}
-
-//出栈并进队列
-LQ PopStack(LQ LQ,node* LS) 
+typedef struct BiTNode
 {
-    node* temp;
-    while (LS != NULL) {
-        // 出栈
-        temp = LS;
-        LS = LS->next;
-        
-        // 将出栈的元素入队
-        temp->next = NULL;
-        LQ.rear->next = temp;
-        LQ.rear = temp;
-    }
-    return LQ;
-}
+char data;
+struct BiTNode *lchild,*rchild;
+}BiTNode,*BiTree;
 
-//出队列并进栈
-node* PopQueue(LQ &LQ,node* LS) 
+//根据先序遍历序列、中序遍历序列，创建一棵二叉树
+BiTree CreateTree(char *preorder, char *inorder, int n)
 {
-    node* temp;
-    
-    // 将队列中的所有元素出队并入栈
-    while (LQ.front->next != NULL) {
-        // 出队
-        temp = LQ.front->next;
-        LQ.front->next = temp->next;
-        
-        // 如果队列变空，更新rear指针
-        if (LQ.front->next == NULL) {
-            LQ.rear = LQ.front;
-        }
-        
-        // 入栈
-        LS = PushStack(LS, temp->data);
-        
-        // 释放出队的结点
-        free(temp);
-    }
-    
-    return LS;
-}
-
-//打印队列全部元素
-void ShowLQ(LQ LQ) {
-    node* tmp = LQ.front->next;
-    while (tmp != NULL) {
-        cout<<tmp->data<<' ';
-        tmp = tmp->next;
-        }
-    cout<<endl;
-}
- 
-int main() 
-{
-    LQ myLQ;
-    int n,e;
-    node* mystack = NULL;
-    myLQ = InitLQ(myLQ);
-    cin>>n;
-    for(int i=0;i<n;i++)
-    {
-    	cin>>e;
-		myLQ=PushQueue(myLQ,e);	
-	}
-    cout<<"原队列:";
-	ShowLQ(myLQ);
-    mystack=PopQueue(myLQ,mystack);
+	if(n == 0)
+		return NULL;
 	
-    myLQ=PopStack(myLQ,mystack); 
-    cout<<"逆置后队列:";
-    ShowLQ(myLQ);
+	// 创建根节点，先序序列的第一个元素是根
+	BiTree root = new BiTNode;
+	root->data = preorder[0];
+	
+	// 在中序序列中找到根节点的位置
+	int i;
+	for(i = 0; i < n; i++)
+	{
+		if(inorder[i] == preorder[0])
+			break;
+	}
+	
+	// i是根节点在中序序列中的位置
+	// 中序序列中，根节点左边的i个元素是左子树，右边的n-i-1个元素是右子树
+	// 先序序列中，第1个元素是根，接下来i个元素是左子树，剩下的是右子树
+	root->lchild = CreateTree(preorder + 1, inorder, i);
+	root->rchild = CreateTree(preorder + i + 1, inorder + i + 1, n - i - 1);
+	
+	return root;
+}
+
+//后序遍历二叉树
+void PostOrder(BiTree T)
+{
+	if(T != NULL)
+	{
+		PostOrder(T->lchild);
+		PostOrder(T->rchild);
+		cout << T->data;
+	}
+}
+
+int main()
+{
+	BiTree T;
+	char pre[100],ino[100];
+	cin>>pre;
+	cin>>ino;
+	int len = strlen(pre);
+	T=CreateTree(pre,ino,len);
+	PostOrder(T);
+    cout<<endl;
     return 1;
 }
